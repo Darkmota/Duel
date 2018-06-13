18bec3159d37ce3050850d6ab4e3b030fbf45a5c
Dwenze <39053008+Dwenze@users.noreply.github.com>
2018-06-14 01:02:44 +0800

    index css框架

--- front-vue/src/components/index.vue
+++ front-vue/src/components/index.vue
@@ -1,14 +1,23 @@
 <template>
-  <div>
   <div class="index">
-    <el-container id="frame">
-      <el-header whatthefuck>
+    <el-header id="whatthefuck">
         <h2>shenmegui</h2>
       </el-header>
+      <el-container id="frame">
+      <el-main id="main">
+        <div class="rate">
+        <el-progress id="win-rate" type="circle" :percentage="80"></el-progress>
+        <h1>胜率</h1>
+        </div>
+        <div class="line"></div>
+      </el-main>
       <el-aside id="acco-info">
+        <h1>帐号信息</h1>
+        
+        <div class="acco-info-line"></div>
+        <h1>胜率</h1>
       </el-aside>
     </el-container>
-  </div>
   </div>
 </template>
 
@@ -45,15 +54,44 @@ export default {
   width: 100%;
   height: 40px;
 }
+#whatthefuck{
+   height:300px;
+}
 #frame{
   width:100%;
-  height:300px;
-  background-color: black;
+  height:600px;
   color: orange;
 }
+#main{
+  height: 600px;
+  background-color:lightgrey;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+}
 #acco-info{
+  margin-left: 20px;
   background-color: lightgrey;
   height:300px;
   width:100%;
+  border-radius: 15px;
+}
+.line{
+  margin-left: 50%;
+  margin-top: -200px;
+  width:1px;
+  height:500px;
+  background-color: white;
+}
+.rate{
+  width:50%;
+}
+#win-rate{
+  margin-top: 100px;
+}
+.acco-info-line{
+  margin-left: 10%;
+  width:220px;
+  height:1px;
+  background-color: white;
 }
 </style>
